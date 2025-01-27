6bb3bc82cf995f4b9c732155469f5b5b23289409
ConanSpithoven <conan.spithoven@gmail.com>
2025-01-27 11:21:00 +0100

    added res.send

--- routes/bosses.mjs
+++ routes/bosses.mjs
@@ -93,11 +93,13 @@ router.delete("/:id", async (req, res) => {
 router.options("/", async (req, res) => {
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.header('Allow', 'GET,POST,OPTIONS');
+  res.send(200);
 });
 
 router.options("/:id", async (req, res) => {
   res.header('Access-Control-Allow-Methods', 'GET, PUT, DELETE, OPTIONS');
   res.header('Allow', 'GET,PUT,DELETE,OPTIONS');
+  res.send(200);
 });
 
 export default router;
